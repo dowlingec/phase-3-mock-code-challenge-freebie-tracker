e7e056cc9f87375d9148c8459d229044e3b9f22e
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-04 16:32:16 -0400

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-Freebie.create(item_name: "Baseball Cap", value: 6, company_id: , dev_id:)
+Freebie.create(item_name: "Baseball Cap", value: 6)
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
